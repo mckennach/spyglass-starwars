5d53ecc738942bd6294949b12b678cadb256be16
Chris McKenna <hello@mckennach.com>
2023-10-30 16:40:48 -0400

    added toggle

--- src/components/header/theme-changer.tsx
+++ src/components/header/theme-changer.tsx
@@ -16,8 +16,15 @@ export default function ThemeChanger()  {
     <div className="text-black dark:text-white">
       <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')} className="opacity-100 hover:text-green-600 active:opacity-60">
         <Image
-            className="h-6 w-auto dark:invert"
-            src={`/${logo}`}
+            className="h-6 w-auto dark:invert hidden dark:block"
+            src={`/jedi.svg`}
+            alt=""
+            width={32}
+            height={32}
+          />
+        <Image
+            className="h-6 w-auto dark:invert dark:hidden"
+            src={`/sith.svg`}
             alt=""
             width={32}
             height={32}
